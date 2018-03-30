2d19bebf3a67a3e350f57ca097c7bcdb16ae8736
DESKTOP-E00DT8O\yuukumo <yuukumo0312@gmail.com>
2018-03-30 18:11:58 +0800

    2018-03-30, temporarily closed

--- tmp/data/scripts/ToCreateFishingTaxonListTW-v01.R
+++ tmp/data/scripts/ToCreateFishingTaxonListTW-v01.R
@@ -10,62 +10,62 @@ library(data.table)
 ## 2013 漁業年報中的版本
 
 # 自 pre-procesed 中匯入資料
-FTXLFATW_2013 <- fread(file = "./pre-processed/FishingTaxonListFATW_2013_01.txt",
-                       skip = 1, header = F, sep = "\t", stringsAsFactors = F,
-                       encoding = "UTF-8")
+FTXL_FATW_2013 <- fread(file = "./pre-processed/FishingTaxonList_FATW_2013_01.txt",
+                        skip = 1, header = F, sep = "\t", stringsAsFactors = F,
+                        encoding = "UTF-8")
 
 # 擷取所需的欄位並為之命名
-FTXLFATW_2013 <- FTXLFATW_2013[, c(1,2,4)]
-names(FTXLFATW_2013) <- c('taxonID', 'vernacularName', 'scientificName')
+FTXL_FATW_2013 <- FTXL_FATW_2013[, c(1,2,4)]
+names(FTXL_FATW_2013) <- c('taxonID', 'vernacularName', 'scientificName')
 
 # 將 scientificName 中的 "sp." 或 "spp." 一律取代為 ""
-FTXLFATW_2013[, scientificName := gsub(" sp.| spp.", replacement = "", scientificName)]
+FTXL_FATW_2013[, scientificName := gsub(" sp.| spp.", replacement = "", scientificName)]
 #
-FTXLFATW_2013[, scientificName := gsub(" (?", replacement = "", scientificName)]
+FTXL_FATW_2013[, scientificName := gsub(" (?", replacement = "", scientificName)]
 
 
 ## 2016 漁業年報中的版本
 
 # 自 pre-procesed 中匯入資料
-FTXLFATW_2016 <- fread(file = "./pre-processed/FishingTaxonListFATW_2016_01.txt",
+FTXL_FATW_2016 <- fread(file = "./pre-processed/FishingTaxonList_FATW_2016_01.txt",
                        skip = 1, header = F, sep = "\t", stringsAsFactors = F,
                        encoding = "UTF-8")
 
 # 擷取所需的欄位並為之命名
-FTXLFATW_2016 <- FTXLFATW_2016[, c(1,2,4)]
-names(FTXLFATW_2016) <- c('taxonID', 'vernacularName', 'scientificName')
+FTXL_FATW_2016 <- FTXL_FATW_2016[, c(1,2,4)]
+names(FTXL_FATW_2016) <- c('taxonID', 'vernacularName', 'scientificName')
 
 # 將 scientificName 中的 "sp." 或 "spp." 一律取代為 ""
-FTXLFATW_2016[, scientificName := gsub(" sp.| spp.", replacement = "", scientificName)]
+FTXL_FATW_2016[, scientificName := gsub(" sp.| spp.", replacement = "", scientificName)]
 
 # 將 scientificName 中以中文頓號 (、) 分隔的方式改為以 "|" 分隔
-FTXLFATW_2016[, scientificName := gsub("、|、 | 、", replacement = "|", scientificName)]
+FTXL_FATW_2016[, scientificName := gsub("、|、 | 、", replacement = "|", scientificName)]
 
 # 新增 taxonRank 欄位並將學名明確且單一之分類群的 taxonRank 設為 species
-FTXLFATW_2016[scientificName %like% " ", taxonRank := "species"]
+FTXL_FATW_2016[scientificName %like% " ", taxonRank := "species"]
 # 將帶有 "var." 之分類群的 taxonRank 設為 variety
-FTXLFATW_2016[scientificName %like% "var.", taxonRank := "variety"]
+FTXL_FATW_2016[scientificName %like% "var.", taxonRank := "variety"]
 # 將學名以 "idae"、"aceae" 結尾之分類群的 taxonRank 設為 family
-FTXLFATW_2016[scientificName %like% "dae$|eae$", taxonRank := "family"]
+FTXL_FATW_2016[scientificName %like% "dae$|eae$", taxonRank := "family"]
 # 將學名以 "formes" 結尾之分類群的 taxonRank 設為 order
-FTXLFATW_2016[scientificName %like% "formes$", taxonRank := "order"]
+FTXL_FATW_2016[scientificName %like% "formes$", taxonRank := "order"]
 
 # 將已完成和未完成 (只能以人工) taxonRank 判定的 taxa 分離出來
-FTXLFATW_2016_A <- FTXLFATW_2016[!is.na(taxonRank)]
-FTXLFATW_2016_B <- FTXLFATW_2016[is.na(taxonRank)][order(scientificName)]
+FTXL_FATW_2016_A <- FTXL_FATW_2016[!is.na(taxonRank)]
+FTXL_FATW_2016_B <- FTXL_FATW_2016[is.na(taxonRank)][order(scientificName)]
 
 # 將必須進行人工判定 taxonRank 的 taxa 匯出做處理
-fwrite(FTXLFATW_2016_B, file = "./in-process/FTXLFATW_2016_B.txt", sep = "\t")
+fwrite(FTXL_FATW_2016_B, file = "./in-process/FTXL_FATW_2016_B.txt", sep = "\t")
 # 處理完畢後再匯入
-FTXLFATW_2016_B <- fread(file = "./in-process/FTXLFATW_2016_B.txt", header = T,
+FTXL_FATW_2016_B <- fread(file = "./in-process/FTXL_FATW_2016_B.txt", header = T,
                          sep = "\t", stringsAsFactors = F,
                          encoding = "UTF-8")
 
 # 將被分割的表單重新合併
-FTXLFATW_2016 <- rbind(FTXLFATW_2016_A, FTXLFATW_2016_B)
+FTXL_FATW_2016 <- rbind(FTXL_FATW_2016_A, FTXL_FATW_2016_B)
 
 # 追加 year 欄位
-FTXLFATW_2016[, year := 2016]
+FTXL_FATW_2016[, year := 2016]
 
 
 

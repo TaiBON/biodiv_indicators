dc63b972b43b0f71045d9015cf9a70eb30724683
Fu-Chun Yang <yuukumo0312@gmail.com>
2018-05-18 03:18:03 +0800

    Visualization of ii.01

--- indicators/visualization/taibon_ii01_1.0/server.R
+++ indicators/visualization/taibon_ii01_1.0/server.R
@@ -9,26 +9,84 @@ mpalist.fatw.vis <- mpalist.fatw[!is.na(basedOn) & !is.na(level) &
                                    !is.na(areaTotalByFATW)][
                                      !(nameOfMPA %in% unique(parentMPA))]
 
+mpalist.fatw.vis[basedOn == "國家公園法", basedOnNew := 1][
+  basedOn == "野生動物保育法", basedOnNew := 2][
+    basedOn == "文化資產保存法", basedOnNew := 3][
+      basedOn == "都市計畫法", basedOnNew := 4][
+        basedOn == "發展觀光條例", basedOnNew := 5][
+          basedOn == "漁業法", basedOnNew := 6]
+
+mpalist.fatw.vis[is.na(areaTerrestrialByFATW), areaTerrestrialByFATW := 0]
+
 denominator <- 65076.96
 
 server <- function(input, output) {
   
-  output$mpa.aera <- renderText(
+  output$mpa_area <- renderText({
     
-    if(input$includeIt_2 == 1) {
-      mpalist.fatw.vis <- mpalist.fatw.vis[nameOfMPA != "相關漁具漁法及特定漁業禁漁區"]
+    if(input$includeIt == 1) {
+      mpalist.fatw.vis <-
+        mpalist.fatw.vis[!is.na(typeOfCase) &
+                           nameOfMPA != "相關漁具漁法及特定漁業禁漁區"]
+    } else if(input$includeIt == 2) {
+      mpalist.fatw.vis <-
+        mpalist.fatw.vis[nameOfMPA != "相關漁具漁法及特定漁業禁漁區"]
+    } else {
+      mpalist.fatw.vis <- mpalist.fatw.vis
     }
     
-    if(input$includeIt_1 == 1) {
-      mpalist.fatw.vis <- mpalist.fatw.vis[!is.na(typeOfCase)]
-    }
-    
-    results <- mpalist.fatw.vis[basedOn %in% input$selectedLaw &
+    results <- mpalist.fatw.vis[basedOnNew %in% input$selectedLaw &
                                   level %in% input$selectedLevel][,
                                     .(area.total = sum(areaTotalByFATW),
-                                      area.terr = sum(areaTerrestrialByFATW))],
-
-    paste(round(((results$area.total - results$area.terr)/denominator)*100, 2), '%')
-  )
+                                      area.terr = sum(areaTerrestrialByFATW))]
+    
+    paste(round(results$area.total, 2), ' - ',
+          round(results$area.terr, 2), ' = ',
+          round(results$area.total - results$area.terr, 2))
+  })
   
+  output$mpa_ratio <- renderText({
+    
+    if(input$includeIt == 1) {
+      mpalist.fatw.vis <-
+        mpalist.fatw.vis[!is.na(typeOfCase) &
+                           nameOfMPA != "相關漁具漁法及特定漁業禁漁區"]
+    } else if(input$includeIt == 2) {
+      mpalist.fatw.vis <-
+        mpalist.fatw.vis[nameOfMPA != "相關漁具漁法及特定漁業禁漁區"]
+    } else {
+      mpalist.fatw.vis <- mpalist.fatw.vis
+    }
+    
+    results <- mpalist.fatw.vis[basedOnNew %in% input$selectedLaw &
+                                  level %in% input$selectedLevel][,
+                                    .(area.total = sum(areaTotalByFATW),
+                                      area.terr = sum(areaTerrestrialByFATW))]
+    
+    paste(round(((results$area.total - results$area.terr)/denominator)*100, 2), '%',
+          sep = "")
+  })
+  
+  output$mpa_twenty <- renderText({
+    
+    if(input$includeIt == 1) {
+      mpalist.fatw.vis <-
+        mpalist.fatw.vis[!is.na(typeOfCase) &
+                           nameOfMPA != "相關漁具漁法及特定漁業禁漁區"]
+    } else if(input$includeIt == 2) {
+      mpalist.fatw.vis <-
+        mpalist.fatw.vis[nameOfMPA != "相關漁具漁法及特定漁業禁漁區"]
+    } else {
+      mpalist.fatw.vis <- mpalist.fatw.vis
+    }
+    
+    results <- mpalist.fatw.vis[basedOnNew %in% input$selectedLaw &
+                                  level %in% input$selectedLevel][,
+                                    .(area.total = sum(areaTotalByFATW),
+                                      area.terr = sum(areaTerrestrialByFATW))]
+    
+    ratioTW <- round(((results$area.total - results$area.terr)/denominator)*100, 2)
+                     
+    paste(ratioTW, '%', ' - ', '20%', ' = ', ratioTW - 20, '%', sep = "")
+  })
 }
--- indicators/visualization/taibon_ii01_1.0/ui.R
+++ indicators/visualization/taibon_ii01_1.0/ui.R
@@ -1,5 +1,4 @@
 library(shiny)
-library(ggplot2)
 library(data.table)
 
 ui <- fluidPage(
@@ -10,12 +9,25 @@ ui <- fluidPage(
     
     sidebarPanel(
       
-      h3(class = 'inputTitle', strong('參數欄')),
+      h3(class = 'inputTitle', strong('指標參數調節欄')),
 
       br(),
       h3(strong('計算面積比時的分子'), class = 'inputTitle2'),
+      br(),
+      
+      div(
+        class = 'single',
+        radioButtons(
+          'includeIt',
+          strong('本計畫採用之資料集，有部分海洋保護區缺乏地理圖資相關資訊，本團隊難以評估其劃設與面積計算方式是否合理，其中包括幅員廣闊、足以顯著影響指標計算結果的「相關漁具漁法及特定漁業禁漁區」。請問是否要將這些海洋保護區納入計算？'),
+          choices = list("先不用，謝謝" = 1,
+                         "請納入「相關漁具漁法及特定漁業禁漁區」以外的部分，謝謝" = 2,
+                         "請全部納入，謝謝" = 3),
+          selected = 1)
+      ),
+      
       checkboxGroupInput('selectedLaw',
-                         label = h4(strong('請勾選所欲納入的 MPA 法令依據'), class = 'inputTitle'),
+                         label = h4(strong('請勾選您認為應納入計算的 MPA 法令依據'), class = 'inputTitle'),
                          choices = list(
                            '國家公園法' = 1,
                            '野生動物保育法' = 2,
@@ -27,43 +39,29 @@ ui <- fluidPage(
                          selected = 1:6
       ),
       checkboxGroupInput('selectedLevel',
-                         label = h4(strong('請勾選所欲納入的 MPA 保護等級'), class = 'inputTitle'),
+                         label = h4(strong('請勾選您認為應納入的 MPA 保護等級'), class = 'inputTitle'),
                          choices = list(
                            '等級一「禁止進入或影響」' = 1,
                            '等級二「禁止採捕」' = 2,
                            '等級三「分區多功能使用」' = 3
                          ),
                          selected = 1:3
-      ),
-      div(
-        class = 'single',
-        radioButtons(
-          'includeIt_1',
-          label = h4(strong('是否納入資料來源中沒有地理圖資相關資訊的 MPA？'), class = 'inputTitle'),
-          choices = list("先不用，謝謝" = 1,
-                         "請直接納入，謝謝" = 2),
-          selected = 1),
-        radioButtons(
-          'includeIt_2',
-          label = h4(strong('是否納入基於漁業法且保護等級為第三級的「相關漁具漁法及特定漁業禁漁區」？（相關問題請見指標詳情）'),
-                     class = 'inputTitle'),
-          choices = list("先不用，謝謝" = 1,
-                         "請直接納入，謝謝" = 2),
-          selected = 1)
       )
     ),
     
     mainPanel(
-      h3('12 浬領海面積'),
+      h3('12 浬領海面積（平方公里）'),
+      p("65076.96", class = "const"),
       hr(),
-      h3('海洋保護區面積'),
+      h3('海洋保護區面積（平方公里）'),
       h4('減去陸域部分和各保護區重疊的部分'),
-      uiOutput('mpa.area'),
+      uiOutput('mpa_area'),
       hr(),
-      h3('海洋保護區佔總水域面積之比率'),
-      uiOutput('mpa.ratio'),
+      h3('海洋保護區佔領海面積之比率'),
+      uiOutput('mpa_ratio'),
       hr(),
-      h3('距愛知目標或 SDGs 預期目標還有')
+      h3('與愛知目標或 SDGs 預期目標 (20%) 的差距'),
+      uiOutput('mpa_twenty')
     )
   )
 )
